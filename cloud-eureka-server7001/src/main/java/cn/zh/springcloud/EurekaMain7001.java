bc81a79570d428069c8aa45399ae937915edb42f
航 周 <18511599658@163.com>
2020-09-08 20:54:11 +0800

    2020springcloud2

---
+++ cloud-eureka-server7001/src/main/java/cn/zh/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package cn.zh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author PSVM
+ * @date 2020/9/7 - 16:24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
